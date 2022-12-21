3193b0788f6d0fad46c3bb37b7d2e17ea0c4c613
yasserhamek <yasser.hamek@gmail.com>
2022-12-21 19:03:43 +0100

    adding report creation end point

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { data, Report, ReportType } from './Data';
+import { v4 as uuidGenerator } from 'uuid';
 
 @Controller('report/:type')
 export class AppController {
@@ -18,5 +27,49 @@ export class AppController {
     return data.report
       .filter((report) => report.reportType === ReportType[type.toUpperCase()])
       .find((report) => report.id === id);
+  }
+
+  @Post()
+  createReport(
+    @Param('type') type: ReportType,
+    @Body() { source, amount }: { source: string; amount: number },
+  ): Report {
+    const createdReport: Report = {
+      id: uuidGenerator(),
+      source: source,
+      amount: amount,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      reportType: ReportType[type.toUpperCase()],
+    };
+
+    data.report.push(createdReport);
+
+    return createdReport;
+  }
+
+  @Post(':id')
+  createReportById() {
+    return 'null';
+  }
+
+  @Put()
+  updateReport() {
+    return 'null';
+  }
+
+  @Put(':id')
+  updateReportById() {
+    return 'null';
+  }
+
+  @Delete(':id')
+  deleteReportById(): void {
+    const constt = null;
+  }
+
+  @Delete()
+  deleteReport(): void {
+    const constt = null;
   }
 }
