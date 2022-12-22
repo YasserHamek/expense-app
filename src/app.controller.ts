848b1260692d5eba5e7d337b3aa76ab23fb27bf8
yasserhamek <yasser.hamek@gmail.com>
2022-12-22 08:39:36 +0100

    adding service layer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,16 +8,17 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { Data, data, Report, ReportType } from './Data';
-import { v4 as uuidGenerator } from 'uuid';
+import { data, Report, ReportType } from './Data';
+
+import { AppService } from './app.service';
 
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getAllReport(@Param('type') type: ReportType): Report[] {
-    return data.report.filter(
-      (report) => report.reportType === ReportType[type.toUpperCase()],
-    );
+    return this.appService.getAllReport(type);
   }
 
   @Get(':id')
@@ -25,9 +26,7 @@ export class AppController {
     @Param('type') type: ReportType,
     @Param('id') id: string,
   ): Report {
-    return data.report
-      .filter((report) => report.reportType === ReportType[type.toUpperCase()])
-      .find((report) => report.id === id);
+    return this.appService.getReportById(type, id);
   }
 
   @Post()
@@ -35,36 +34,20 @@ export class AppController {
     @Param('type') type: ReportType,
     @Body() { source, amount }: { source: string; amount: number },
   ): Report {
-    const createdReport: Report = {
-      id: uuidGenerator(),
-      source: source,
-      amount: amount,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      reportType: ReportType[type.toUpperCase()],
-    };
-
-    data.report.push(createdReport);
-
-    return createdReport;
+    return this.appService.createReport(type, { source, amount });
   }
 
   @Put(':id')
   updateReportById(
     @Param('id') id: string,
-    @Body() body: { source: string; amount: number },
+    @Body() { source, amount }: { source: string; amount: number },
   ): void {
-    data.report.forEach((report) => {
-      if (report.id === id) {
-        report.amount = body.amount;
-        report.source = body.source;
-      }
-    });
+    this.appService.updateReportById(id, { source, amount });
   }
 
   @HttpCode(202)
   @Delete(':id')
   deleteReportById(@Param('id') id: string): void {
-    data.report = data.report.filter((report) => report.id != id);
+    this.appService.deleteReportById(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { data, Report, ReportType } from './Data';
+import { v4 as uuidGenerator } from 'uuid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Nest!';
+  getAllReport(type: ReportType): Report[] {
+    return data.report.filter(
+      (report) => report.reportType === ReportType[type.toUpperCase()],
+    );
+  }
+
+  getReportById(type: ReportType, id: string): Report {
+    return data.report
+      .filter((report) => report.reportType === ReportType[type.toUpperCase()])
+      .find((report) => report.id === id);
+  }
+
+  createReport(
+    type: ReportType,
+    { source, amount }: { source: string; amount: number },
+  ): Report {
+    const createdReport: Report = {
+      id: uuidGenerator(),
+      source: source,
+      amount: amount,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      reportType: ReportType[type.toUpperCase()],
+    };
+
+    data.report.push(createdReport);
+
+    return createdReport;
+  }
+
+  updateReportById(id: string, body: { source: string; amount: number }): void {
+    data.report.forEach((report) => {
+      if (report.id === id) {
+        report.amount = body.amount;
+        report.source = body.source;
+      }
+    });
+  }
+
+  deleteReportById(id: string): void {
+    data.report = data.report.filter((report) => report.id != id);
   }
 }
